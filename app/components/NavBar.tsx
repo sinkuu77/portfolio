21362357b6d21ff5425fc4d9fb66032d9fbe3301
Minyoung GWON <sinkuu@naver.com>
2023-11-06 21:15:14 +0100

    Ajouté une vidéo comme l'arrière-plan

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,2 +1,23 @@
 'use client'
+import Link from 'next/link'
 
+export default function NavBar() { 
+    return (
+        <nav className='absolute left-24 right-24 w-80 h-80 bg-white'>
+            <ul>
+                <li>
+                    <Link href='/profile'>Profile</Link>
+                </li>
+                <li>
+                    <Link href='/projects'>Projects</Link>
+                </li>
+                <li>
+                    <Link href='/skills'>Skills</Link>
+                </li>
+                <li>
+                    <Link href='/contact'>Contact</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from 'next/image';
+import NavBar from './components/NavBar';
 
 export default function Home() {
   return (
-    <main>
-      <header>
-        <h1>GWON Minyoung</h1>
-        <h2>Front-End <br />Développeur</h2>
-      </header>
-    </main>
-  )
+    <>
+      <main className="w-full h-full relative">
+        <video autoPlay loop muted playsInline className="absolute inset-0 w-full h-full object-cover -z-1">
+          <source src="/ferris_wheel.mp4" type="video/mp4" />
+        </video>
+        <header className='p-6'>
+          <div className="relative text-lg decoration-yellow">
+            <h1><strong>GWON Minyoung</strong></h1>
+            <h2>Front-End <br />Développeur</h2>
+          </div>
+        </header>
+        <NavBar/>
+      </main>
+    </>
+  );
 }
