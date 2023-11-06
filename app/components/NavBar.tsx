9aa87f8900916953613a18d6577e1b3f3726b442
Minyoung GWON <sinkuu@naver.com>
2023-11-06 21:33:37 +0100

    Implémenté le composant de navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 
 export default function NavBar() { 
     return (
-        <nav className='absolute left-24 right-24 w-80 h-80 bg-white'>
-            <ul>
+        <nav className='absolute left-24 right-24 w-80 h-80 bg-white opacity-50 rounded-xl p-6 '>
+            <ul className='decoration-green text-2xl font-bold h-full flex flex-col gap-12'>
                 <li>
                     <Link href='/profile'>Profile</Link>
                 </li>
