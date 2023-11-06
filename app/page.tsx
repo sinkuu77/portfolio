035a0b2e82ae3791b866a545cbbbaeb268aa1658
Minyoung GWON <sinkuu@naver.com>
2023-11-06 16:51:37 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <main>
+      <header>
+        <h1>GWON Minyoung</h1>
+        <h2>Front-End <br />Développeur</h2>
+      </header>
+    </main>
+  )
+}
