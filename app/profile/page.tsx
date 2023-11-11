d29524b4eb2167fa3744d0ba8bfdf24b7da49bb3
Minyoung GWON <sinkuu@naver.com>
2023-11-11 17:29:16 +0100

    Implémenté le style basique au Profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,9 +2,14 @@
 export default function Profile() { 
     return (
         <>
-        <div className='bg-main h-screen w-2/4'>
+        <section className='h-screen w-full flex'>
+            <div className='bg-main h-screen w-2/4'>
+                <h1 className='text-main text-5xl m-6'><strong>Profile</strong></h1>
+            </div>
+            <div className='h-screen w-2/4'>
 
-        </div>
+            </div>
+        </section>
         </>
     )
 }
