c8081d1319449a46f82d4075ec3d5002b699fe85
Minyoung GWON <sinkuu@naver.com>
2023-11-11 21:30:16 +0100

    Ajouté svg images

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -16,6 +16,13 @@ export default function Profile() {
                     ></TypeIt>
                 </h2>
             </div>
+            <div className='relative h-4/6 w-full'>
+                <div className="custom-shape-divider-top-1699730016">
+                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                        <path d="M0,0V7.23C0,65.52,268.63,112.77,600,112.77S1200,65.52,1200,7.23V0Z" className="shape-fill"></path>
+                    </svg>
+                </div>
+            </div>
             <div className='my-11'>
                 <div className='w-3/5 flex flex-col m-auto gap-7 text-justify'>
                     <p className='text-2xl'>
