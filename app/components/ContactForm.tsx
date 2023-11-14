f79f37d27b91b5dc1946487d17c343fa49dfa742
Minyoung GWON <sinkuu@naver.com>
2023-11-14 20:12:02 +0100

    Implémenté la fonctionnalité d'envoyer un email

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -1,19 +1,38 @@
+import React, { FormEvent, useRef } from 'react';
+import emailjs from 'emailjs-com';
+
 
 export default function ContactForm() { 
+    const form = useRef<HTMLFormElement>(null);
+
+    const sendEmail = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const currentForm = form.current;
+    if(currentForm === null) return;
+
+    emailjs.sendForm('service_s4g2q6p', 'template_8ktvrrh', currentForm, 'rIfaJ4iHbkj67vTK2')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    e.currentTarget.reset();
+    };
     return (
         <div className='flex flex-col justify-center items-center p-12'>
-            <form>
+            <form  ref={form} onSubmit={sendEmail}>
                 <div className='flex flex-col gap-2 text-gray-700 w-[650px]'>
                     <label className='text-2xl'>Nom</label>
-                    <input type='text' name='nom' className='rounded h-[40px] p-2'/>
+                    <input type='text' name='name' className='rounded h-[40px] p-2' required/>
                 </div>
                 <div className='flex flex-col gap-2 text-gray-700 w-[650px]'>
                     <label className='text-2xl'>E-mail</label>
-                    <input type='email' name='email' className='rounded h-[40px] p-2' />
+                    <input type='email' name='email' className='rounded h-[40px] p-2'  required/>
                 </div>
                 <div className='flex flex-col gap-2 text-gray-700 w-[650px]'>
                     <label className='text-2xl'>Message</label>
-                    <textarea name='message' rows={10} cols={30} className='rounded p-2'></textarea>
+                    <textarea name='message' rows={10} cols={30} className='rounded p-2' required></textarea>
                 </div>
                 <div className='flex items-center justify-center m-10'>
                     <button className='text-2xl px-10 py-1.5 rounded-full email-btn'>
