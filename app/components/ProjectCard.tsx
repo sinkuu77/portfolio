2037d2ad36221e743ba9a5a5cb2905002179c9d9
Minyoung GWON <sinkuu@naver.com>
2023-11-15 20:43:40 +0100

    Implémenté la maquette de la page de Projects

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -5,6 +5,7 @@ interface Project {
     id: string,
     title: string,
     cover: string,
+    alt: string,
     contexte: string,
     problematique: string,
     github: string,
@@ -13,7 +14,7 @@ interface Project {
 
 
 export default function ProjectCard() { 
-    const [projects, setProjects] = useState<any[]>([])
+    const [projects, setProjects] = useState<Project[]>([])
 
     useEffect(() => {
         fetch('/projectData.json')
@@ -25,15 +26,32 @@ export default function ProjectCard() {
     
     return (
         <>
-            <ul>
+            <ul className='w-full flex gap-8 justify-center items-center ml-48'>
                 {projects.map((project, index) => (
                     <div
+                    className='mt-28'
                     key={`${project.id}-${index}`}
                     >
                         <img 
+                        className='w-[520px] h-[280px] rounded-3xl object-cover shadow-xl'
                         src={project.cover}
                         alt={project.alt}
                         />
+                        <p
+                        className='text-white text-2xl mt-2'
+                        >
+                            {project.title}
+                        </p>
+                        <ul className='flex gap-3'>
+                            {project.tags.map((tag:string, tagIndex:number) => (
+                                <li
+                                key={`${tag}-${tagIndex}`}
+                                className='bg-yellow-300 rounded px-2 mt-2 text-gray-700'
+                                >
+                                    {tag}
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 ))}
             </ul>
