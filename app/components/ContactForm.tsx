e8019a2191f51ef6e270726012f5a0c5fcbabc6e
Minyoung GWON <sinkuu@naver.com>
2023-11-23 15:07:07 +0100

    Ajouté for aux labels

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -29,16 +29,16 @@ export default function ContactForm() {
         <div className='flex flex-col justify-center items-center p-12'>
             <form  ref={form} onSubmit={sendEmail}>
                 <div className='flex flex-col gap-2 text-gray-700 w-[650px] md:w-[300px]'>
-                    <label className='text-2xl md:text-xl'>Nom</label>
-                    <input type='text' name='name' className='rounded h-[40px] p-2' required/>
+                    <label className='text-2xl md:text-xl' htmlFor='name'>Nom</label>
+                    <input type='text' id='name' name='name' className='rounded h-[40px] p-2' required/>
                 </div>
                 <div className='flex flex-col gap-2 text-gray-700 w-[650px] md:w-[300px]'>
-                    <label className='text-2xl md:text-xl'>E-mail</label>
-                    <input type='email' name='email' className='rounded h-[40px] p-2'  required/>
+                    <label className='text-2xl md:text-xl' htmlFor='email'>E-mail</label>
+                    <input type='email' id='email' name='email' className='rounded h-[40px] p-2'  required/>
                 </div>
                 <div className='flex flex-col gap-2 text-gray-700 w-[650px] md:w-[300px]'>
-                    <label className='text-2xl md:text-xl'>Message</label>
-                    <textarea name='message' rows={10} cols={30} className='rounded p-2' required></textarea>
+                    <label className='text-2xl md:text-xl' htmlFor='message'>Message</label>
+                    <textarea id='message 'name='message' rows={10} cols={30} className='rounded p-2' required></textarea>
                 </div>
                 <div className='flex items-center justify-center m-10'>
                     <button className='text-2xl px-10 py-1.5 rounded-full email-btn md:text-base'>
