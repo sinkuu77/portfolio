3eee597d247e19af128aa4c5d6e45862ae8f81f5
Minyoung GWON <sinkuu@naver.com>
2023-11-24 13:22:49 +0100

    Ajouté des balises meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,13 @@ import { Karla } from 'next/font/google'
 import type { Metadata } from 'next'
 
 import Cursor from './components/Cursor'
-
+import Head from 'next/head'
 
 const karla = Karla({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'GWON Minyoung portfolio',
-  description: 'Bienvenu à mon site',
+  title: 'GWON Minyoung portfolio frontend developer',
+  description: 'Un site de portfolio pour GWON Minyoung, développeuse frontend à Strasbourg, actuellement à la recherche de son premier emploi dans ce domaine.',
 }
 
 export default function RootLayout({
@@ -20,6 +20,31 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
+      <Head>
+        <title>
+          {metadata.title as React.ReactNode}
+        </title>
+        <meta name='description' content={metadata.description?? 'Default Description'} />
+        <meta property="og:title" content="GWON Minyoung portfolio frontend developer" key="ogtitle" />
+        <meta property="og:description" content={metadata.description?? 'Default Description'} key="ogdesc" />
+        <meta
+            property="og:image"
+            content="/img/site.png"
+            key="ogimage"
+        />
+        <meta
+            property="og:site_name"
+            content="portfolio frontend | GWON Minyoung"
+            key="ogsitename"
+        />
+        <meta
+            property="og:url"
+            content="https://portfolio-blue-nine-67.vercel.app/"
+            key="ogurl"
+        />
+        <meta property="og:type" content="website" key="ogtype" />
+        <meta name="twitter:card" content={metadata.description?? 'Default Description'} />
+      </Head>
       <body className={karla.className}>
         <Cursor />
         {children}
