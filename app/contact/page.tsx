dfa1a649d3631df883cd564c367333ee3ddb460a
Minyoung GWON <sinkuu@naver.com>
2023-11-29 21:40:45 +0100

    Résolu un bug de responsive

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,10 +7,10 @@ import ScrollToTopIcon from '../components/ScrollToTopIcon'
 export default function Contact() { 
     return (
         <section id='contact' className='w-full h-screen overflow-hidden'>
-            <div className='h-1/5 w-full'>
+            <div className='h-[10%] w-full'>
                 <h1 className='text-gray-700 text-5xl m-6 md:text-3xl'><strong>Contact</strong></h1>
             </div>
-            <div className='relative h-4/5 w-full bg-main'>
+            <div className='relative h-[90%] w-full bg-main'>
                 <ContactForm />
                 <ScrollToTopIcon />
             </div>
